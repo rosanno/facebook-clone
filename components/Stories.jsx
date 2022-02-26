64ae3fde3154d7eb5463c19c2511b8c0e555da0d
rosanno <rosannomacabuhay@gmail.com>
2022-02-26 09:16:18 +0800

    Update data

--- components/Stories.jsx
+++ components/Stories.jsx
@@ -1,34 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import StoryCard from './StoryCard';
 
-// const stories = [
-//   {
-//     name: 'Sonny Sangha',
-//     src: 'https://links.papareact.com/zof',
-//     profile: 'https://links.papareact.com/l4v',
-//   },
-//   {
-//     name: 'Elon Musk',
-//     src: 'https://links.papareact.com/4zn',
-//     profile: 'https://links.papareact.com/KxK',
-//   },
-//   {
-//     name: 'Jeff Bezoz',
-//     src: 'https://links.papareact.com/k2j',
-//     profile: 'https://links.papareact.com/f0p',
-//   },
-//   {
-//     name: 'Mark Zuckerberg',
-//     src: 'https://links.papareact.com/xql',
-//     profile: 'https://links.papareact.com/snf',
-//   },
-//   {
-//     name: 'Bill Gates',
-//     src: 'https://links.papareact.com/4u4',
-//     profile: 'https://links.papareact.com/zvy',
-//   },
-// ];
-
 function Stories() {
   const [stories, setStories] = useState([]);
 
